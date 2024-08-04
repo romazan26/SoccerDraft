5725a06b4807d714be273b59fa6dda057846f9fa
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-08-04 22:57:46 +0300

    Initial Commit

---
+++ SoccerDraft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoccerDraft
+//
+//  Created by Роман on 04.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
